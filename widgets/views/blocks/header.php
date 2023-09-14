3849b3dbbd9f3816dd226f1fb9dbb465aca275b2
lobzenko <ms@lobzenko.ru>
2023-09-14 12:31:06 +0700

    up

--- widgets/views/blocks/header.php
+++ widgets/views/blocks/header.php
@@ -1,6 +1,6 @@
 <div class="container">
   <p class="pre-header"><?=$sub_title?></p>
-  <?php if (empty((string)$h) || $h!='h1'){?>
+  <?php if (empty($h) || $h!='h1'){?>
   <h2 class="mb-5"><?=$title?></h2>     
   <?php }else {?>
   <h1 class="mb-5"><?=$title?></h1>
