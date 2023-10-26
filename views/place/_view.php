3210357443b0f1bb711ec9e690a331ee8b9cb83c
lobzenko <ms@lobzenko.ru>
2023-10-26 15:15:14 +0700

    up

--- views/place/_view.php
+++ views/place/_view.php
@@ -1,6 +1,6 @@
 <div class="col-md-4 mb-4">
   <a href="/place/<?=$model->id_place?>" class="card">
-    <img src="<?=(!empty($model->media))?$model->media->showThumb(['w'=>'350']):''?>" class="card-img-top" alt="">
+    <img src="<?=(!empty($model->media))?$model->media->showThumb(['w'=>350,'h'=>400]):''?>" class="card-img-top" alt="">
     <div class="card-body">
         <h5 class="card-title"><?=$model->name?></h5>
         <p class="card-text"><?=$model->address?></p>
