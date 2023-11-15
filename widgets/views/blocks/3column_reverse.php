c817a7607683224f17a8f970621da3eed73190df
lobzenko <ms@lobzenko.ru>
2023-11-15 14:30:06 +0700

    up

--- widgets/views/blocks/3column_reverse.php
+++ widgets/views/blocks/3column_reverse.php
@@ -4,11 +4,11 @@
 
     <div class="row ">      
       <div class="col-md-4 my-4 my-md-0">
-        <?php if (!empty($image->media)){?><img class="w-100 h-100 fit-cover" src="<?=$image->media->showThumb(['w'=>350])?>" alt=""><?php }?>
+        <?php if (!empty($image->media)){?><img class="w-100 h-100 fit-cover" src="<?=$image->media->showThumb(['w'=>700])?>" alt=""><?php }?>
       </div>
       <div class="col-md-6  d-flex flex-column justify-content-end">
         <p><?=$description?></p>
-        <?php if (!empty($image2->media)){?><img src="<?=$image2->media->showThumb(['w'=>540])?>" alt=""><?php }?>
+        <?php if (!empty($image2->media)){?><img src="<?=$image2->media->showThumb(['w'=>1080])?>" alt=""><?php }?>
       </div>      
     </div>
 </div>
