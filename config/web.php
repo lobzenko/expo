99e17fecc4f019378a2a7e33c7b7afef3e2beea0
MSD <msd_86@mail.ru>
2024-02-12 03:59:18 +0700

    up

--- config/web.php
+++ config/web.php
@@ -55,11 +55,11 @@ $config = [
             // for the mailer to send real emails.
             'transport' => [
                 'class' => 'Swift_SmtpTransport',
-                'host' => 'mail.sfu-kras.ru',
+                'host' => 'https://mail.sfu-kras.ru',
                 'username' => 'expo@sfu-kras.ru',
                 'password' => '36dc30d0',
                 'port' => '465',
-                //'encryption' => 'tls',
+                'encryption' => 'tls',
             ],
             'useFileTransport' => false,
         ],
